b3967ae749145b20dcb915d164cd75d80eada8f4
afarstic <144730389+afarstic@users.noreply.github.com>
2024-07-07 09:01:59 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 var score = 0;
+var s = 5;
+var p = 1;
 
-function Click(el) {
-    score++;
+function Click() {
+    score+=p;
     document.getElementById("score").innerHTML = "" + score;
+}
+function Upgrade() {
+    if (score >= s) {
+        score -= s;
+        s *= 2;
+        p++;
+        document.getElementById("up").innerHTML = "" + s;
+    }
 }
